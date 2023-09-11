c565187224304fb7c1d50c8d483d4924ba369a57
cfuselli <carlo@fuselli.net>
2023-09-11 22:19:45 +0200

    add website_12

--- websites/website_12.py
+++ websites/website_12.py
@@ -31,7 +31,7 @@ def scrape_website(html):
         img_tag = article.find('img')
         if img_tag and 'src' in img_tag.attrs:
             house.images.append(img_tag['src'])
-            
+
 
         # Address and City
         address_container = article.find('h2', class_='property_name')
@@ -64,12 +64,12 @@ website = Website(url, example_html, scrape_website)
 
 
 # Run the scrape_example function to test the scraper
-houses = website.scrape_example()
+# houses = website.scrape_example()
 
 
-# # Print the results
-for house in houses[::-1]:
-    house.print()
-    print()
+# # # Print the results
+# for house in houses[::-1]:
+#     house.print()
+#     print()
 
 
