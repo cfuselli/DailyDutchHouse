48beb4a6fe70120de0ca556616a1bd2ab90383b0
cfuselli <cfuselli@nikhef.nl>
2024-04-14 16:16:56 +0200

    java websites

--- test_selenium2.py
+++ test_selenium2.py
@@ -1,7 +1,4 @@
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-options = Options()
-options.BinaryLocation = "/usr/bin/chromium-browser"
-
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",options=options)
-driver.get("https://www.google.com")
+opts = Options()
+opts.binary_location = chromium_path
+driver = webdriver.Chrome(chrome_options=opts)
