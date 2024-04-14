d6bfd60b6b2545bee12ca5af025f3c8b30f997fd
cfuselli <cfuselli@nikhef.nl>
2024-04-14 16:34:48 +0200

    java websites

--- test_selenium2.py
+++ test_selenium2.py
@@ -1,6 +1,51 @@
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+import chromedriver_binary  # Adds chromedriver binary to path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+import os
+# Set up Chrome options for headless mode
+options = Options()
 
-opts = Options()
-opts.binary_location = '/usr/bin/chromium-browser'
-driver = webdriver.Chrome(chrome_options=opts)
+# if on my mac, do nothing, if on the server, set the binary location
+#
+# if the location of home is /home/runner, then it is on the server
+if os.path.expanduser('~') == '/home/carlo': 
+    print('On the server')
+    options.binary_location = '/usr/bin/chromium-browser'  # or '/snap/bin/chromium' if the other doesn't work
+else:
+    pass
+
+
+options.add_argument("--headless")  # Ensure headless mode is activated
+options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration
+options.add_argument("--no-sandbox")  # Bypass OS security model, required on many systems
+options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+options.add_argument("enable-automation")  # Avoid popup about automated software
+options.add_argument("--disable-infobars")  # Disable info bars from Chrome
+options.add_argument("--disable-extensions")  # Disable extensions
+options.add_argument("--window-size=1920,1080")  # Specify browser resolution
+
+# Initialize WebDriver
+try:
+    service = Service(ChromeDriverManager().install())
+except:
+    service = Service(ChromeDriverManager('123.0.6312.105').install())
+
+
+driver = webdriver.Chrome(service=service, options=options)
+
+# Load the webpage
+driver.get('https://www.pararius.nl/huurwoningen/amsterdam')
+
+# Wait for JavaScript to load and for the page to be fully rendered
+time.sleep(2)  # Adjust sleep time based on the specific needs and response times of the website
+
+# Now you can scrape the content
+content = driver.page_source
+print(content)  # Output the page content or process it as needed
+
+# Clean up
+driver.quit()
