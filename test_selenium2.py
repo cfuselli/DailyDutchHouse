a31388c1843399907e9c4e358c93f84940a74644
cfuselli <cfuselli@nikhef.nl>
2024-04-14 16:17:43 +0200

    java websites

--- test_selenium2.py
+++ test_selenium2.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
 opts = Options()
 opts.binary_location = '/usr/bin/chromium-browser'
 driver = webdriver.Chrome(chrome_options=opts)
